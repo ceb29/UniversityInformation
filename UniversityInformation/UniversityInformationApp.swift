ded86f2286d3873a0a98f29d1b4dfe3aacf55704
Curtis Becker <ceb2941@gmail.com>
2022-07-27 10:57:27 -0400

    Initial Commit

---
+++ UniversityInformation/UniversityInformationApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversityInformationApp.swift
+//  UniversityInformation
+//
+//  Created by admin on 7/27/22.
+//
+
+import SwiftUI
+
+@main
+struct UniversityInformationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
